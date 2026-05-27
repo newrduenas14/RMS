ace9b80b08a3d6dddc8115725b0185e838d00824
newrduenas14 <newrduenas14@gmail.com>
2026-05-26 22:33:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Hotel Mis Sueños — GitHub frontend configuration
 // Paste your deployed Apps Script Web App URL here after deploying the API.
 window.HMS_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbztEj3ylzBm9QKzmT79_75GqMUmgIJ5qJ8oDs9MxHK43Yz0AfY8Ov42K8Ij0TEjTLdwyQ/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbwyxRTfuReVG-V4wnLkwr5DJ8Fn9yGVsusHDiOD5FG8yEmiQJJqmhU4Vq2ZKgszGzhnPg/exec'
 };
